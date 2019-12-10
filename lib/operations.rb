69969d63ad7003c44d970e649729e035c529f739
Tammy Lam <lamtammy1515@gmail.com>
2019-12-10 02:30:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,7 +6,8 @@ end
 
 
 def not_safe?(speed)
-	
+	(Going 30 mph on the freeway would be not_safe, as would going 95 mph.).to eq(true)
+(Going 50 miles per hour, however, would return false as that's within the "not_safe" range.).to eq(true)
 end
 	
 
